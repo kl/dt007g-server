b110df42cbe2ec9c035ddd48d71c0efba59a1afe
Kalle Lindström <lindstrom.kalle@gmail.com>
2012-10-29 07:26:36 +0100

    Added server.properties configuration file

--- src/chatserver/ChatToAlice.java
+++ src/chatserver/ChatToAlice.java
@@ -8,37 +8,37 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
-public class ChatToAlice
-{
-  static Socket socket;
-  private static PrintWriter out;
-  private static BufferedReader in;
+public class ChatToAlice {
 
-  public void GetAliceResponse(Socket clientsocket, String m)
-  {
-    String[] dm = m.split(">", 4);
-    String message = dm[3].replaceAll("<", "").replaceAll(">", "");
-    try
-    {
-      InetAddress host = InetAddress.getByName("193.10.119.33");
+    static Socket socket;
+    private static PrintWriter out;
+    private static BufferedReader in;
 
-      socket = new Socket(host.getHostName(), 23456);
-      out = new PrintWriter(socket.getOutputStream(), true);
-      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      out.println(message);
-      System.out.println("Sent to Alice:   " + message);
-      String response = in.readLine();
-      DataOutputStream dos = new DataOutputStream(clientsocket.getOutputStream());
-      dos.writeUTF("<PRIVATE><ALICE><" + response + ">");
-      System.out.println("Replied Alice, :   " + response);
-      socket.close();
-      socket = null;
-      out = null;
-      in = null;
+    public void GetAliceResponse(Socket clientsocket, String m) {
+
+        String[] dm = m.split(">", 4);
+        String message = dm[3].replaceAll("<", "").replaceAll(">", "");
+
+        try {
+            String aliceIP = Main.properties.getProperty("alice_ip");
+            InetAddress host = InetAddress.getByName(aliceIP);
+
+            socket = new Socket(host.getHostName(), Integer.parseInt(Main.properties.getProperty("alice_port")));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out.println(message);
+            System.out.println("Sent to Alice:   " + message);
+            String response = in.readLine();
+            DataOutputStream dos = new DataOutputStream(clientsocket.getOutputStream());
+            dos.writeUTF("<PRIVATE><ALICE><" + response + ">");
+            System.out.println("Replied Alice, :   " + response);
+            socket.close();
+            socket = null;
+            out = null;
+            in = null;
+        }
+        catch (Exception ex) {
+          ex.printStackTrace();
+        }
     }
-    catch (Exception ex)
-    {
-      ex.printStackTrace();
-    }
-  }
 }
--- src/chatserver/Main.java
+++ src/chatserver/Main.java
@@ -1,53 +1,72 @@
 package chatserver;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main
 {
-  public static ConcurrentHashMap<String, Friend> userList;
-  public static ConcurrentHashMap<String, Socket> socketList;
-  public static File userlogfile;
-  public static String syspath;
-  public static String currentsong;
 
-  public static void main(String[] args)
-  {
-    System.out.println("Chat Server is Running");
-    syspath = System.getProperty("user.dir");
-    System.out.println("Logs are being written to:  " + syspath);
-    userList = new ConcurrentHashMap();
-    socketList = new ConcurrentHashMap();
-    currentsong = "";
-    try
+    public static ConcurrentHashMap<String, Friend> userList;
+    public static ConcurrentHashMap<String, Socket> socketList;
+    public static File userlogfile;
+    public static String syspath = System.getProperty("user.dir");
+    public static String currentsong;
+
+    public static final String PROPERTIES_FILE_PATH = syspath + "/server.properties";
+    public static Properties properties;
+
+    public static void main(String[] args)
     {
-      ServerSocket ss = new ServerSocket(8000);
-      userList.put("ALICE", new Friend("ALICE", "Alice the Robot", InetAddress.getLocalHost(), "NONE; I AM JUST A ROBOT", "2010/10/08 10:36:11"));
+        readProperties();
 
-      Thread singingthread = new Thread(new SongPlayer());
-      singingthread.start();
-      while (true)
-      {
-        Socket s = ss.accept();
+        System.out.println("Chat Server is Running");
 
-        System.out.println(s.getInetAddress().getHostAddress());
+        System.out.println("Logs are being written to: " + syspath);
+        userList = new ConcurrentHashMap<String, Friend>();
+        socketList = new ConcurrentHashMap<String, Socket>();
+        currentsong = "";
 
-        Thread th = new Thread(new ClientThread(s));
+        try {
+            int port = Integer.parseInt(properties.getProperty("server_port"));
+            ServerSocket ss = new ServerSocket(port);
+            System.out.println("Listening on port: " + port);
 
-        th.start();
-      }
+            userList.put("ALICE", new Friend("ALICE", "Alice the Robot", InetAddress.getLocalHost(), "NONE; I AM JUST A ROBOT", "2010/10/08 10:36:11"));
 
+            Thread singingthread = new Thread(new SongPlayer());
+            singingthread.start();
+
+            while (true) {
+                Socket s = ss.accept();
+
+                System.out.println(s.getInetAddress().getHostAddress());
+
+                Thread th = new Thread(new ClientThread(s));
+
+                th.start();
+            }
+        }
+        catch (IOException ex) {
+              Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    catch (IOException ex)
-    {
-      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+
+    private static void readProperties() {
+        try {
+            FileInputStream stream = new FileInputStream(PROPERTIES_FILE_PATH);
+
+            properties = new Properties();
+            properties.load(stream);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(0);
+        }
     }
-  }
 }
